ab0f8b6b46d0eea56e10022e5ab4ace099bd3e9f
vishnudasok <vishnudasok2580@gmail.com>
2024-09-19 16:15:35 +0530

    Admin bar changed

--- src/AdminComponents/Admin.jsx
+++ src/AdminComponents/Admin.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link, Outlet, NavLink } from 'react-router-dom'
 import axios from 'axios'
 
 import Sidebar from '../adminPages/Sidebar'
 function Admin() {
-   const functionz=()=>{
-         
-   }
+  const functionz = () => {
+
+  }
   return (
-    <div className='flex-row'>
-      <Sidebar/>
-  </div>
-  
-      
+    <div className='flex w-full bg-pink-300 h-dvh '>
+      <div className='w-1/6 h-full bg-slate-300 flex flex-col p-8 gap-3 max-sm:bg-blue-700 max-lg:bg-green-900'>
+        <NavLink to='/Admin/dashboard'>Dashboard</NavLink>
+        <NavLink to='/Admin/customers'>Customer</NavLink>
+        <NavLink to='/Admin/orders'>Order</NavLink>
+        <NavLink to='/Admin/owner'>Owner</NavLink>
+      </div>
+      <div >
+        <Outlet />
+      </div>
+    </div>
+
+
   )
 }
 
--- src/AdminPages/dashboard.jsx
+++ src/AdminPages/dashboard.jsx
@@ -1,13 +1,10 @@
 // src/components/Dashboard.jsx
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Dashboard() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-      <p>Welcome to the admin dashboard!</p>
-      {/* Add dashboard statistics, charts, and analytics here */}
-    </div>
+    <div>Dashboard</div>
   );
 }
 
